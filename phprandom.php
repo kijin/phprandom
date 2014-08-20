93a3ba5c94f0a8af63f9f367f09dcabe19488b6b
Kijin Sung <kijin@kijinsung.com>
2014-08-20 21:52:56 +0900

    Fix negative integers in 32-bit PHP

--- phprandom.php
+++ phprandom.php
@@ -40,7 +40,7 @@ class PHPRandom
     {
         $bytes_required = min(4, ceil(log($max - $min, 2) / 8) + 1);
         $bytes = self::getBinary($bytes_required);
-        $offset = hexdec(bin2hex($bytes)) % ($max - $min);
+        $offset = abs(hexdec(bin2hex($bytes)) % ($max - $min));
         return floor($min + $offset);
     }
     
