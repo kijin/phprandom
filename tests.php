eea34585d2eda6b6eb86049c53608500796a33bc
Kijin Sung <kijin@kijinsung.com>
2014-08-20 18:43:56 +0900

    Improve unit tests to measure the distribution of random values better

--- tests.php
+++ tests.php
@@ -6,67 +6,110 @@ class PHPRandom_UnitTest extends PHPUnit_Framework_TestCase
 {
     public function test_integer_without_range()
     {
-        $count = 18000;
-        $tolerance = 0.8;
+        $count = 10000;
         $buckets = array_fill(0, 256, 0);
         for ($i = 0; $i < $count; $i++)
         {
             $number = PHPRandom::getInteger();
             $bucket_index = $number >> 23;
+            $this->assertEquals(true, isset($buckets[$bucket_index]));
             $buckets[$bucket_index]++;
         }
+        
+        $distribution = 0;
         for ($i = 0; $i < 256; $i++)
         {
-            $this->assertGreaterThan(($count / 256) * (1 - $tolerance), $buckets[$i]);
-            $this->assertLessThan(($count / 256) * (1 + $tolerance), $buckets[$i]);
+            if ($buckets[$i] > ($count / 256) * 0.5) $distribution++;
+            if ($buckets[$i] < ($count / 256) * 1.5) $distribution++;
         }
+        $this->assertGreaterThan(256, $distribution);
     }
     
     public function test_integer_with_range()
     {
-        $count = 12000;
-        $tolerance = 0.6;
+        $count = 10000;
         $buckets = array_fill(100, 101, 0);
         for ($i = 0; $i < $count; $i++)
         {
             $number = PHPRandom::getInteger(100, 200);
+            $this->assertEquals(true, isset($buckets[$number]));
             $buckets[$number]++;
         }
+        
+        $distribution = 0;
         for ($i = 100; $i < 200; $i++)
         {
-            $this->assertGreaterThan(($count / 100) * (1 - $tolerance), $buckets[$i]);
-            $this->assertLessThan(($count / 100) * (1 + $tolerance), $buckets[$i]);
+            if ($buckets[$i] > ($count / 100) * 0.5) $distribution++;
+            if ($buckets[$i] < ($count / 100) * 1.5) $distribution++;
         }
-        $this->assertEquals($buckets[200], 0);
+        $this->assertGreaterThan(100, $distribution);
     }
     
     public function test_string()
     {
-        for ($i = 1; $i <= 600; $i++)
+        $count = 2000;
+        $buckets = array_combine(str_split('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), array_fill(0, 62, 0));
+        for ($i = 1; $i <= $count; $i++)
         {
-            $str = PHPRandom::getString($i);
-            $this->assertEquals(strlen($str), $i);
-            $this->assertRegExp('/^[0-9a-zA-Z]+$/', $str);
+            $len = mt_rand(2, 256);
+            $str = PHPRandom::getString($len);
+            $this->assertEquals($len, strlen($str));
+            $this->assertEquals(true, isset($buckets[$str[0]]));
+            $buckets[$str[0]]++;
         }
+        
+        $distribution = 0;
+        foreach ($buckets as $key => $value)
+        {
+            if ($value > ($count / 62) * 0.5) $distribution++;
+            if ($value < ($count / 62) * 1.5) $distribution++;
+        }
+        $this->assertGreaterThan(62, $distribution);
     }
     
     public function test_hex_string()
     {
-        for ($i = 1; $i <= 600; $i++)
+        $count = 2000;
+        $tolerance = 0.6;
+        $buckets = array_combine(str_split('0123456789abcdef'), array_fill(0, 16, 0));
+        for ($i = 1; $i <= $count; $i++)
         {
-            $str = PHPRandom::getHexString($i);
-            $this->assertEquals(strlen($str), $i);
-            $this->assertRegExp('/^[0-9a-f]+$/', $str);
+            $len = mt_rand(2, 256);
+            $str = PHPRandom::getHexString($len);
+            $this->assertEquals($len, strlen($str));
+            $this->assertEquals(true, isset($buckets[$str[0]]));
+            $buckets[$str[0]]++;
         }
+        
+        $distribution = 0;
+        foreach ($buckets as $key => $value)
+        {
+            if ($value > ($count / 16) * 0.5) $distribution++;
+            if ($value < ($count / 16) * 1.5) $distribution++;
+        }
+        $this->assertGreaterThan(16, $distribution);
     }
     
     public function test_binary()
     {
-        for ($i = 1; $i <= 200; $i++)
+        $count = 2000;
+        $tolerance = 0.9;
+        $buckets = array_fill(0, 256, 0);
+        for ($i = 1; $i <= $count; $i++)
         {
-            $str = PHPRandom::getRandomBytes($i);
-            $this->assertEquals(strlen($str), $i);
+            $len = mt_rand(2, 256);
+            $str = PHPRandom::getRandomBytes($len);
+            $this->assertEquals($len, strlen($str));
+            $buckets[ord($str[0])]++;
         }
+        
+        $distribution = 0;
+        foreach ($buckets as $key => $value)
+        {
+            if ($value > ($count / 256) * 0.5) $distribution++;
+            if ($value < ($count / 256) * 1.5) $distribution++;
+        }
+        $this->assertGreaterThan(256, $distribution);
     }
     
     public function test_list_sources()
